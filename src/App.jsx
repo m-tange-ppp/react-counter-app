0b1360728d3e19100dc4f5c5677a7de5fb30d009
MiTange <tmrevo210@yahoo.co.jp>
2024-08-11 18:38:04 +0900

    色々なCounterを作れるようにして、countによってレイアウトを変えるように

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import Counter from "./components/Counter";
 
 export default function App() {
+    const plusN = (n, count) => {
+        return count + n;
+    };
+
+    function multipleN(n, count) {
+        return count * n;
+    };
+
     return (
         <div>
-            <h1 className="text-2xl text-center m-4">
+            <h1 className="text-4xl text-center m-4 font-bold bg-gradient-to-r from-pink-500 to-violet-700 bg-clip-text text-transparent">
                 React Counter App
             </h1>
-            <div className="flex gap-2">
-                <Counter />
-                <Counter />
+            <div className="flex flex-wrap gap-2">
+                <Counter method={plusN} n={1} start={0} />
+                <Counter method={plusN} n={2} start={0} />
+                <Counter method={multipleN} n={2} start={1} />
+                <Counter method={plusN} n={3} start={0} />
+                <Counter method={multipleN} n={-3} start={1} />
             </div>
         </div>
     )
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
+export default function Counter(props) {
+    const [count, setCount] = useState(props.start);
     return (
-        <div className="text-center w-40 bg-blue-500 border border-blue-900 rounded-xl">
-            <button className="" onClick={() => { setCount(count + 1) }}>Click Me !!</button>
-            <p>{count}</p>
-            <button onClick={() => { setCount(0) }}>Reset</button>
+        <div className={`flex flex-col items-center justify-center gap-2 p-2 w-[calc(50%-0.25rem)] border-neutral-900 rounded-xl bg-gradient-to-r ${count % 2 == 0 ? `from-blue-600 to-violet-700` : `from-yellow-600 to-red-700`}`}>
+            <button className=" h-12 rounded-xl bg-neutral-950 px-6 text-neutral-50 shadow-lg shadow-neutral-500/20 transition active:scale-95" onClick={() => { setCount(props.method(props.n, count)) }}>Click Me !!</button>
+            <p className="flex h-12 w-16 text-4xl rounded-xl items-center justify-center ">{count}</p>
+            <button className="relative overflow-hidden rounded-xl bg-neutral-950 px-5 py-2.5 text-white duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-110 active:scale-y-90" onClick={() => { setCount(props.start) }}>Reset</button>
         </div>
     )
 }
